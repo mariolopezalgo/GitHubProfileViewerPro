20e134d6000d0ac68e17ff8d3f264032ddb55821
mariolopezalgo <mariolopezalgo@gmail.com>
2021-04-23 17:33:40 -0600

    local storage

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
   console.log('Window Ready.');
+  $('#card').hide();
+
+  $("#letsGo").on("click", function(e) {
+    e.preventDefault();
+    localStorage.clear();
+    var userName = $("#userName").value();
+    localStorage.setItem("userName", userName);
 });
+  //   $('#result').toggle();
+  //   $('#avatar').toggle();
+  //   $('#pp').toggle();
+});
+
+
+// $.ajax({
+//   url: 'https://api.github.com/users',
+//   method: 'GET',
+// }).then((res) => {
+//   var data = res;
+//   console.log(data);
+// });
+
+// https://avatars.githubusercontent.com/u/79119798?v=4
--- js/userProfile.js
+++ js/userProfile.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-
+    var userName = localStorage.getItem("userName");
+    $.ajax({
+        url: 'https://api.github.com/users/' + userName,
+        method: 'GET',
+      }).then((res) => {
+        var data = res;
+        console.log(data);
+        $('#card').show(); 
+        $('#result').text(data.login);
+        $('#avatar').attr('src', data.avatar_url);
+      });
 });
