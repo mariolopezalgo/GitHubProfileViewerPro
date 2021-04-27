0fa4b805a85664bb1f08aa5cbdc1ac409993198d
mariolopezalgo <mariolopezalgo@gmail.com>
2021-04-26 21:28:28 -0600

    done

--- js/index.js
+++ js/index.js
@@ -2,7 +2,25 @@ $(document).ready(function () {
   localStorage.clear();
   console.log('Window Ready.');
   $(document).on('mousemove', function (e) {
-    $('#coords').html('    Page Coords: Y: ' + e.clientY + '    X: ' + e.clientX);
+    $('#coords').html(
+      '    Page Coords: Y: ' + e.clientY + '    X: ' + e.clientX
+    );
+  });
+
+  $('#userName').mouseenter(function () {
+    $('#userName').css({ 'background-color': 'pink' });
+  });
+  
+  $('#userName').mouseleave(function () {
+    $('#userName').css({ 'background-color': 'red' });
+  }); 
+
+  // $('#letsGo').mouseenter(function () {
+  //   $('#letsGo').animate();
+  // });
+
+  $('input').keyup(function (e) {
+    console.log(e.target.value);
   });
 
   $('#letsGo').on('click', function (e) {
